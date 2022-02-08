6047ae42b25e7107d8180818efcd105b41fc5dc7
NTPhat-99 <nguyenthuanphat.it@gmail.com>
2022-02-08 15:31:46 +0700

    new project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import CountrySeletor from "./components/CountrySelector";
+import Highlight from "./components/Highlight";
+import Summary from "./components/Summary";
+
+
+function App() {
+  return (
+    <>
+      <CountrySeletor />
+      <Highlight />
+      <Summary />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CountrySelector/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CountrySeletor() {
+  return <div>
+      This is CountrySeletor
+  </div>;
+}
